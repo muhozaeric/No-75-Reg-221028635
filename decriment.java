99e9b4cfad9a9d919035bbcf8ec7fe13efc1037d
muhozaeric <149691487+muhozaeric@users.noreply.github.com>
2023-11-04 05:20:57 -0700

    Add files via upload

---
+++ decriment.java
@@ -0,0 +1,10 @@
+//the program will dislpay number from fifrty to one
+public class decriment {
+    public static void main(String[]args){//main method
+        for(int i=50;i>1;i--){
+            System.out.println(i);//output excution
+        }
+
+    }
+    
+}
---
+++ dowhileloop2.java
@@ -0,0 +1,16 @@
+//the program multiply two numbers
+public class dowhileloop2 {
+  public static void main(String[]args){//main method
+    //declaration and initialization
+    int a=50;
+    int b=5;
+    int mult;
+    do
+    {
+        mult=a*b;//body of the loop
+    }
+    while(a>50);{//condition
+    System.out.println("Multiplication:" + mult);//print output
+    }
+  }  
+}
