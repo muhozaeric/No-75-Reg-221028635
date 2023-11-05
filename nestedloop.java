94e58afd72c5e66b1eab71fbd85385d9cf40e8aa
muhozaeric <149691487+muhozaeric@users.noreply.github.com>
2023-11-05 04:25:10 -0800

    Add files via upload

---
+++ nestedloop.java
@@ -0,0 +1,11 @@
+public class nestedloop{  
+public static void main(String[] args) {  
+//loop of w  
+for(int w=10;w<=20;w++){  
+//loop of g 
+for(int g=4;g<=13;g++){  
+        System.out.println(w+" "+g);  
+}//end of w  
+}//end of g 
+}  
+}  
